965e1871f89bdaaa797f46986805c05acd422c1d
Kevin Li <kevinlig@gmail.com>
2020-10-05 18:34:02 -0700

    Oops last commit didn't capture the new files, just the modified ones

---
+++ Assets/Menu/MenuController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject menuPanel;
+    public GameObject introPanel;
+
+    public void OnClickIntro() {
+        menuPanel.SetActive(false);
+        introPanel.SetActive(true);
+    }
+
+    public void OnClickPlay() {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void OnClickLicense() {
+
+    }
+
+    public void CloseIntro() {
+        menuPanel.SetActive(true);
+        introPanel.SetActive(false);
+    }
+}
---
+++ Assets/Menu/TutorialController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+public class TutorialController : MonoBehaviour
+{
+    public MenuController menuController;
+    public GameObject[] pages;
+    public IntReactiveProperty currentPage = new IntReactiveProperty(0);
+
+    void Start() {
+        currentPage
+            .Subscribe((pageIdx) => {
+                int idx = 0;
+                foreach(GameObject page in pages) {
+                    bool active = false;
+                    if (idx == pageIdx) {
+                        active = true;
+                    }
+                    page.SetActive(active);
+
+                    idx++;
+                }
+            });
+            
+    }
+
+    void OnEnable() {
+        currentPage.SetValueAndForceNotify(0);
+    }
+
+
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            if (currentPage.Value + 1 >= pages.Length) {
+                menuController.CloseIntro();
+            } else {
+                currentPage.SetValueAndForceNotify(currentPage.Value + 1);
+            }
+        }
+    }
+
+
+}
